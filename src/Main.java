71d5a4a1cb273bcb77c55df710d492ddba6b6aa1
jordonjones <jordon01jones@gmail.com>
2026-01-03 20:44:35 -0600

    function15

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,19 @@
+// Password Validator
+// Write a function taking a as password
+// and returning `boolean` if: `String`
+
+import java.util.Scanner;
+
 public class Main {
+    public static boolean checkPswd(String p){
+        String password = "money" ;
+        return ( password.equals(p) ) ;
+
+    }
+    public static void main(String[] args){
+        System.out.println (checkPswd("money"));
+
+    }
+
+
 }
